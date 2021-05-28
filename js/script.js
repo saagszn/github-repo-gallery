4284836d78a26e576de540b419e6bac8c1be734b
aeneas_h <ashemp90@gmail.com>
2021-05-28 10:29:31 -0400

    added repo list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // profile info
 const overview = document.querySelector(".overview");
 const username = "saagszn";
+const repoList = document.querySelector(".repo-list");
 
 const fetchUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -25,4 +26,20 @@ const displayUserInfo = function (data) {
         </div>
     `;
     overview.append(div)
+    gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
